d2c5ee7ee4af1f9c2b0f6447f46e3ff711d10b9c
Joe <jomonjoshygeorge@gmail.com>
2024-01-24 22:39:33 +0530

    cleaned code

--- test/multi_directional_horizontal_list_test.dart
+++ test/multi_directional_horizontal_list_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:multi_directional_horizontal_list/multi_directional_horizontal_list.dart';
-
 void main() {
   test('new test', () {
 
